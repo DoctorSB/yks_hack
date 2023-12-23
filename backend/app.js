95aca105c61143eb4d83ef8797143e5ba63e9160
Ilya Kozionov <ik.ilyakozionov@yandex.ru>
2023-12-23 09:33:09 +0300

    refactor: TASK-15 Рефакторинг АПИ и compose

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,16 @@
 const fastify = require('fastify')({
   logger: true
 });
-const { redis } = require('./services/redis');
-const cors = require('@fastify/cors');
 
-fastify.register(require('@fastify/websocket'));
-
-fastify.register(cors, {
+fastify.register(require('@fastify/cors'), {
   origin: '*',
   methods: 'GET,PUT,POST,DELETE,OPTIONS'
 });
+
 fastify.register(require('./routes/root'));
 
 const start = async () => {
   try {
-    await redis.connect();
     await fastify.listen({ port: 3000, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
--- backend/routesLogic/getFromRedis.js
+++
@@ -1,6 +0,0 @@
-const { hGetAll } = require('../services/redis');
-
-module.exports = async (req, reply) => {
-  const data = await hGetAll(String(req.query.key));
-  reply.send(data);
-};
